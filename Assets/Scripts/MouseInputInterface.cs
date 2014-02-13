f7702094d6ab53050637f0e6e4a0bf6a45b59f02
Patrik Lundqvist <pat.lundqvist@gmail.com>
2014-02-13 17:13:10 +0100

    Changed mouse movement

--- Assets/Scripts/MouseInputInterface.cs
+++ Assets/Scripts/MouseInputInterface.cs
@@ -2,15 +2,17 @@
 using System.Collections;
 
 public class MouseInputInterface : MonoBehaviour {
-	
-	float ballDistanceFromCamera;
 
-	// calculation variables
+	// The mouse sense
+	float mouseSpeed = 1f;
+
+	// Calculation variables
 	float current_x = 0.0f;
 	float current_y = 0.0f;
 	
+	Transform cachedTransform;
 
-	// string references to speed string manipulation/calculation
+	// String references to speed string calculation
 	string x_axis = "Horizontal";
 	string y_axis = "Vertical";
 
@@ -19,47 +21,48 @@ public class MouseInputInterface : MonoBehaviour {
 	
 	// Use this for initialization
 	public void Start () {
-		Screen.showCursor = false;
-		ballDistanceFromCamera = Mathf.Abs(transform.position.z - Camera.main.transform.position.z);
 
 		// Calculate the screen width
 		screen_width = Camera.main.orthographicSize * Camera.main.aspect;
+
 	}
 		
 	// Update is called once per frame
 	public void Update () {
-			
-		if (DidMove()) {
-			Vector3 v3Pos = Input.mousePosition;
+		MouseMove();
+	}
 
-			// Get distance the paddle is in front of the camera
-			v3Pos.z = ballDistanceFromCamera;
-			v3Pos = Camera.main.ScreenToWorldPoint (v3Pos);
+	void Awake() {
+		// Initialize the current x with the current change in x axis
+		current_x = Input.GetAxis(x_axis);
+		current_y = Input.GetAxis(y_axis);
+	}
+
+	void MouseMove()
+	{
+		// Get the current change in horizontal and vertical mouse movement
+		current_x = Input.GetAxis(x_axis);
+		current_y = Input.GetAxis(y_axis);
+
+		// Check if the current x and y has changed
+		if (current_x != 0 || current_y != 0)
+		{
+			// Cache the current position
+			cachedTransform = transform;
+			// Move the cached position with mouse movement
+			cachedTransform.Translate(-current_x * Time.deltaTime * 16.5f * mouseSpeed, current_y * Time.deltaTime * 16.5f  * mouseSpeed, 0);
+
+			// Get the xyz position data from the cached position
+			Vector3 v3Pos = cachedTransform.position;
+
+			// Get the new postition to the ball with a constraint of the screen width and height
 			v3Pos.x = Mathf.Clamp(v3Pos.x, -screen_width +  (renderer.bounds.size.x / 2) , screen_width -  (renderer.bounds.size.x / 2));
 			v3Pos.y = Mathf.Clamp(v3Pos.y, -7 +  (renderer.bounds.size.y / 2) , 10 -  (renderer.bounds.size.y / 2));
+
+			// Apply the position to the ball
 			transform.position = v3Pos;
 		}
 
-	}
 
-	//initialize your variables here
-	void Awake() {
-		// initialize your current x with the current change in x axis
-		current_x = Input.GetAxis(x_axis);
-		current_y = Input.GetAxis(y_axis);
-	}
-
-	bool DidMove() {
-		// get the current change in horizontal mouse movement
-		current_x = Input.GetAxis(x_axis);
-		current_y = Input.GetAxis(y_axis);
-
-	
-
-		// check if the current x has a value of 0. if not, return true;
-		if (current_x == 0 && current_y == 0)
-			return false;
-		else
-			return true;
 	}
 }
